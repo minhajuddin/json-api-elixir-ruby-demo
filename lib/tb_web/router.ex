d0218e8294a5a86ff6e2e2c322ab240687b04dd7
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2018-03-12 22:07:57 +0530

    scaffold

--- lib/tb_web/router.ex
+++ lib/tb_web/router.ex
@@ -2,25 +2,28 @@ defmodule TBWeb.Router do
   use TBWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", TBWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", TBWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", TBWeb do
+    pipe_through(:api)
+    resources("/posts", PostController, except: [:new, :edit])
+    resources("/comments", CommentController, except: [:new, :edit])
+  end
 end
