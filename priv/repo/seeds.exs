6077fc65d7f14556fce55af6f11c031382ad2540
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2018-03-12 22:11:36 +0530

    added seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias TB.Repo
+alias TB.Blog.{Post, Comment}
+
+post = Repo.insert! %Post{title: "Hello, World!", body: "My first blog post"}
+
+
+Repo.insert! %Comment{body: "Wow, nice post", post_id: post.id}
+Repo.insert! %Comment{body: "meh.", post_id: post.id}
+Repo.insert! %Comment{body: "Good stuff", post_id: post.id}
